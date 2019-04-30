894908caea7c50b808f7157f8a0147c65379977c
Jason Leach <jlspursfan@gmail.com>
2019-04-30 21:11:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function add() {
+  
+}
+
+function subtract() {
+  
+}
+
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function makeInt(n) {
+  
+}
+
+function preserveDecimal(n) {
+  
+}
